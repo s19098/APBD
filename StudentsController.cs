c1aa7ac6b8a2f0b0f952846c7806df94fa798aa6
s19098 <61733298+s19098@users.noreply.github.com>
2020-03-23 23:38:13 +0100

    Add files via upload

---
+++ StudentsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MailKit.Search;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public string IndexNumber { get; private set; }
+
+        
+        [HttpGet]
+        public string GetStudent()
+        {
+        return $"Kowalski, Malewski, Andrzejewski sortowanie={OrderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono STUDENTA !");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+    }
+}
+
