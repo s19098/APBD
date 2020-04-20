c247c8c074b50326a3731fb49793153c18cf55ee
s19098 <61733298+s19098@users.noreply.github.com>
2020-04-21 00:59:40 +0200

    Delete PromoteStudentRequest.cs

--- Classes5/Classes5/Models/DTOs/Requests/PromoteStudentRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Classes5.Models.DTOs.Requests
-{
-    public class PromoteStudentRequest
-    {
-        public string Studies { get; set; }
-
-        public int Semester { get; set; }
-    }
-}
