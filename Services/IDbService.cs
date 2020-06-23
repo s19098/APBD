8d3a90690564b3d8621151a04857bd9f7ea28791
s19098 <61733298+s19098@users.noreply.github.com>
2020-06-23 15:59:34 +0200

    Kolokwium1Poprawa

---
+++ Services/IDbService.cs
@@ -0,0 +1,9 @@
+﻿using Kolokwium1Poprawa.DTOs.Responses;
+namespace Kolokwium1Poprawa.Services
+{
+    public class IDbService
+    {
+        public ProjectResponse DeleteProject(int idProject) { }
+        public TeamMemberResponse GetTeamMember(int idMember) { }
+    }
+}
